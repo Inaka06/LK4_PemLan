910b754563d5a61720703be27229180e124d051e
iqbalfahmi-dev <fahmiiqbal398@gmail.com>
2026-03-30 14:23:19 +0700

    testing

--- Codes/src/Interfaces.java
+++ Codes/src/Interfaces.java
@@ -13,3 +13,5 @@ interface LayananInternasional extends Transaksi {
 interface TransferGlobal extends TransaksiDigital, LayananInternasional {
     boolean kirimGlobal(String negaraTujuan, double jumlah);
 }
+
+//test
